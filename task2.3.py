547f41d45b35d3a45f408d679e15a59f58ede69e
zimarinvitalii <zimarin96@gmail.com>
2020-12-22 16:08:46 +0200

    задание 2.3

---
+++ task2.3.py
@@ -0,0 +1,24 @@
+'''
+Using python as a calculator.
+
+Make a program with 2 numbers saved in separate variables a and b, then print the result for each of the following:
+
+Addition
+Subtraction
+Division
+Multiplication
+Exponent (Power)
+Modulus
+Floor division
+'''
+
+a = 9
+b = 3
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a ** b)
+print(abs(-a))
