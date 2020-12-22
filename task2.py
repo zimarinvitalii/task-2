ddf525cd7455e65135a1f9be56511045e9afc332
zimarinvitalii <zimarin96@gmail.com>
2020-12-22 14:24:33 +0200

    initial commit

---
+++ task2.py
@@ -0,0 +1,19 @@
+'''
+Create a python program named “task2”, and use the built-in function `print` in it several times.
+Try to pass “sep”, “end” params and pass several parameters separated by commas.
+Also, provide a comment text above each print statement, mentioned above, with the expected output after execution of the particular print statement.
+'''
+
+#111
+print(111)
+
+#15
+x = 30
+x -= 15
+print(x)
+
+#Guten Morgen!
+print('Guten Morgen!')
+
+#я пью пиво, они пьют пиво, мы пьем пиво!
+print('я пью', 'они пьют', 'мы пьем', sep=' пиво, ', end= ' пиво!')
