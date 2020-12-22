591584fb4a73022296680b8667658fa82ea5c400
zimarinvitalii <zimarin96@gmail.com>
2020-12-22 15:51:52 +0200

    задание 2.2

---
+++ task2.2.py
@@ -0,0 +1,13 @@
+'''
+Task 2
+
+Manipulate strings.
+
+Save your first and last name as separate variables,
+then use string concatenation to add them together with a white space in between and print a greeting.
+'''
+
+first_name = 'Vitalii'
+last_name = 'Zymarin'
+full_name = first_name + ' ' + last_name
+print(f"Hello! My name is {full_name}")
