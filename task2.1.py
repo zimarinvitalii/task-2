ac61882fedd0533bc3ddeaa95c09124f4c5c22b5
zimarinvitalii <zimarin96@gmail.com>
2020-12-22 14:59:29 +0200

    ничего

--- task2.1.py
+++ task2.1.py
@@ -1,2 +1,13 @@
-if __name__ == '__main__':
-    print(3*6)
+'''Make a program that has your name and the current day of the week stored as separate variables and then prints a message like this:
+
+     “Good day <name>! <day> is a perfect day to learn some python.”
+Note that <name> and <day> are predefined variables in source code.
+
+An additional bonus will be to use different string formatting methods for constructing result string.
+'''
+
+name = 'Vitalii'
+day = 'Tuesday'
+print(f'Good day {name}! {day} is a perfect day to learn some python.')
+print('Good day {0}! {1} is a perfect day to learn some python.'.format(name, day))
+print('Good day %s! %s is a perfect day to learn some python.' % (name, day))
